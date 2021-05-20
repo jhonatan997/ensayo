fdd6b8be6e497fee2ed719ecbff9cbe3345c3d35
jhonatan hernandez vallejo <jhonherva@hotmail.com>
2021-05-20 10:56:46 -0500

    ensayo completo

---
+++ manejoSql/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace manejoSql
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgMascotas = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgMascotas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgMascotas
+            // 
+            this.dgMascotas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgMascotas.Location = new System.Drawing.Point(0, 0);
+            this.dgMascotas.Name = "dgMascotas";
+            this.dgMascotas.Size = new System.Drawing.Size(424, 196);
+            this.dgMascotas.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(428, 321);
+            this.Controls.Add(this.dgMascotas);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgMascotas)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgMascotas;
+    }
+}
+
---
+++ manejoSql/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace manejoSql
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)     
+        {
+            dgMascotas.DataSource = ConsultarMAscotas();
+        }
+        public DataTable ConsultarMAscotas()
+        {
+            SqlConnection ObjCn = new SqlConnection("Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=dbMascotas;Data Source=LAPTOP-SS08P4TL");
+            SqlCommand ObjCmd = new SqlCommand("uspConsultaMascotas", ObjCn);//se utiliza cuando hay transacciones (insert,delete,update)
+            ObjCmd.CommandType = CommandType.StoredProcedure; //commantext que le traigo es un sp
+            ObjCn.Open();
+            SqlDataReader Dr = ObjCmd.ExecuteReader(); //para ejecutar consultas (comando)
+            DataTable dt = new DataTable();
+            dt.Load(Dr);
+            ObjCn.Close(); //cerrar la base de datos
+            return dt;
+
+        }
+    }
+
+}
